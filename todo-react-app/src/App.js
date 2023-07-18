c421b82334561ddfd3c2acf1e3cfd4706549619e
NehaS101 <solankineha297@gmail.com>
2023-07-18 15:13:32 +0530

    working on app.js

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import {BrowserRouter as Router,Route,Link} from 'react-router';
+import TodoList from './TodoList';
+import TodoItem from './TodoItem';
+import CreateTodo from './CreateTodo';
 
 function App() {
+  const [todo,setTodos] = useState([]);
   return (
     <div className="App">
      
